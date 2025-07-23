2419bfd857746df76f915693156a70998768f674
Shubkin1isp22 <163159716+Shubkin1isp22@users.noreply.github.com>
2025-07-23 20:54:07 +0700

    version - 0.3 TextBox с ошибкой для ссылки. + лёгкий front-end

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def download(url):
     }
     yt = YoutubeDL(options)
     if is_url(url):
+        
         try:    
             with yt as video:
                 video.download(url)
@@ -30,13 +31,39 @@ def is_url(url):
     print("IS_URL running")
     if bool(re.match('https://', url)):
         return True
+    f_error_box()
+    return False
+    
+
+
+
+def f_error_box():
+    print("error_box is running")
+    text_error_url = "Вы ввели некорректную ссылку.\nПример корректной ссылки:\nhttps://www.youtube.com/watch?v=YIEvDwq41OY\n"
+    error_box.insert("1.0", text_error_url)
+    error_box.get("1.0", "end")
+    error_box.configure(state='disabled')
+    error_box.pack(pady = (15,0))
+    print("error_box is end")
+
+
+
+tk.set_default_color_theme("dark-blue")
 
 win = tk.CTk()
-win.geometry("300x450")
-input = tk.CTkEntry(win, placeholder_text="Введите ссылку:")
-input.pack(pady=7)
+win.geometry("300x400")
+win.title("Video Downloader")
+win.configure(fg_color="#FCE6E6")
 
-button = tk.CTkButton(win, text= "Скачать",fg_color="darkgreen", command=lambda: download(input.get()))
+input = tk.CTkEntry(win, border_color="#FFD1D1", placeholder_text="Введите ссылку:")
+input.pack(pady=(20,5), anchor = "center")
+
+button = tk.CTkButton(win, text= "Скачать", command=lambda: download(input.get()))
 button.pack(pady=5)
+
+label = tk.CTkLabel(win, text = "by shekspii", text_color="grey")
+label.pack(side = "bottom", pady = 5)
+
+error_box = tk.CTkTextbox(win, width=260, height=80, text_color="red", activate_scrollbars=False)
 print("Programm is run")
 win.mainloop()
